c08dee2cf482ec4766ec5d55206bac47b763d5d9
Erfan Saberi <erfansaberiow@gmail.com>
2023-04-16 16:46:02 +0330

    Added error messages

--- app.py
+++ app.py
@@ -11,14 +11,19 @@ with open("DrugPredictionModel.pkl", 'br') as model_file:
 @app.route('/predict', methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
-        age = request.form['age']
-        sex = request.form['sex']
-        bp = request.form['bp']
-        cholesterol = request.form['cholesterol']
-        na_to_k = request.form['na_to_k']
-        
-        prediction = model.predict([[age, sex, bp, cholesterol, na_to_k]])
-        return render_template("predict.html", data={"prediction": prediction})
+        try:
+            age = request.form['age']
+            sex = request.form['sex']
+            bp = request.form['bp']
+            cholesterol = request.form['cholesterol']
+            na_to_k = request.form['na_to_k']
+            
+            prediction = model.predict([[age, sex, bp, cholesterol, na_to_k]])[0]
+            return render_template("predict.html", data={"prediction": prediction})
+        except KeyError:
+            return render_template("predict.html", data={"error": "Please enter all the attributes"})
+        except Exception as e:
+            return render_template("predict.html", data={"error": "An error occured"})
     return render_template("predict.html", data={})
     
 if __name__ == '__main__':
