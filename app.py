2712dd5d76ed596a108891700dbc01a626550953
Erfan Saberi <erfansaberiow@gmail.com>
2023-04-16 16:47:47 +0330

    Code formatting

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 with open("DrugPredictionModel.pkl", 'br') as model_file:
     model = pickle.load(model_file)
-    model:DecisionTreeClassifier
-    
+    model: DecisionTreeClassifier
+
+
 @app.route('/', methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
@@ -17,14 +18,16 @@ def predict():
             bp = request.form['bp']
             cholesterol = request.form['cholesterol']
             na_to_k = request.form['na_to_k']
-            
-            prediction = model.predict([[age, sex, bp, cholesterol, na_to_k]])[0]
+
+            prediction = model.predict(
+                [[age, sex, bp, cholesterol, na_to_k]])[0]
             return render_template("predict.html", data={"prediction": prediction})
         except KeyError:
             return render_template("predict.html", data={"error": "Please enter all the attributes"})
         except Exception as e:
             return render_template("predict.html", data={"error": "An error occured"})
     return render_template("predict.html", data={})
-    
+
+
 if __name__ == '__main__':
     app.run("localhost", 5000, debug=True)
